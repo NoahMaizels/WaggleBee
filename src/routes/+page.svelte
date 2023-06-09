81890c8c562a8efb1fc0dcf326ba858b3cafc7ed
noahniuwa <noahmaizels@protonmail.com>
2023-06-09 15:31:32 +0700

    first

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  /** @type {import('./$types').PageData} */    export let data;
+
+  const paste = async (message) => {
+    const res = await fetch(`https://api.gateway.ethswarm.org/bzz`, {
+      method: 'POST',
+      body: message,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+    const json = await res.json()
+    return json.reference
+  }
+
+  let input = ""
+  let reference = ""
+  const handleClick = async () => {
+    reference = await paste(input)
+  }
+</script>
+
+<h1>{data.text}</h1>
+<div>{reference}</div>
+
+<input bind:value={input}>
+<button on:click={handleClick}>
+	Paste
+</button> 
