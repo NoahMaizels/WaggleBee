bd26fa09e93d8812c14a9d2fd284bf790cc38d5b
noahniuwa <noahmaizels@protonmail.com>
2023-06-13 14:21:48 +0700

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,26 @@
 <script>
   import { onMount } from 'svelte';
-  import {browser} from "$app/environment";
+  // import { browser } from "$app/environment";
   import { page } from '$app/stores';
+  import { Bee } from "@ethersphere/bee-js"
+  const bee = new Bee('https://api.gateway.ethswarm.org')
 
   let text =""
   onMount(async () => {
-    const searchParams = browser && $page.url.searchParams
+    const searchParams = $page.url.searchParams
     let ref = searchParams.get("ref")
-    let res = await fetch(`https://api.gateway.ethswarm.org/bzz/${ref}`)
-    text = await res.text()
+    let res = await bee.downloadFile(ref)
+    text = res.data.text()
 	});
 
-  export async function load({ fetch }) {
-    if (ref) {
-      let res = await fetch(`https://api.gateway.ethswarm.org/bzz/${ref}`)
-      let text = await res.text()
-        return { text }
-    }
-  }
-  
   const paste = async (message) => {
     text = ""
     const res = await fetch(`https://api.gateway.ethswarm.org/bzz`, {
       method: 'POST',
       body: message,
       headers: {
-        'Content-Type': 'text/plain',
+        'Content-Type': 'text/plain;',
+        
       },
     })
     const json = await res.json()
@@ -47,4 +42,5 @@
 <button on:click={handleClick}>
 	Paste
 </button> 
+
 
